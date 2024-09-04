aa80aad85d204dd79f6d1474fff2eed21db4a405
Tyler Olsen <tylerkolsen@gmail.com>
2024-09-04 13:26:35 -0600

    completed steps 1 and 2

--- animal-shelter.js
+++ animal-shelter.js
@@ -1,1 +1,35 @@
-const animalData = require('./animal-data.json');
+const animalData = require("./animal-data.json");
+
+class Animal {
+  constructor(name, species, color, hunger = 50) {
+    this.name = name;
+    this.species = species;
+    this.color = color;
+    this.hunger = hunger;
+  }
+
+  greet() {
+    console.log(`Hi ${this.name} the ${this.species}`);
+  }
+
+  feed() {
+    this.hunger -= 20;
+    console.log(`Yum, I love food`);
+  }
+}
+
+class AnimalShelter {
+  constructor() {
+    this.animals = [];
+  }
+
+  addAnimal(animal) {
+    this.animals.push(animal);
+  }
+
+  adopt(animal) {
+    // this.animals = this.animals.filter((ani) => ani.name !== animal.name);
+    const animalIndex = this.animals.indexOf(animal);
+    this.animals.splice(animalIndex, 1);
+  }
+}
