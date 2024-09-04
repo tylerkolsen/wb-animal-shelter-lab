385cc2fde62da1d5ce2dacf30b1456e12e1f9173
Tyler Olsen <tylerkolsen@gmail.com>
2024-09-04 14:36:09 -0600

    completed lab

--- animal-shelter.js
+++ animal-shelter.js
@@ -8,13 +8,46 @@ class Animal {
     this.hunger = hunger;
   }
 
+  greet(greeting = "Hi") {
+    console.log(`${greeting}, I'm ${this.name} the ${this.species}`);
+  }
+
+  feed(food = "food") {
+    this.hunger -= 20;
+    console.log(`Yum, I love ${food}`);
+  }
+}
+
+class Cat extends Animal {
+  constructor(name, color, hunger = 50) {
+    super(name, "cat", color, hunger);
+    this.food = "fish";
+  }
+
   greet() {
-    console.log(`Hi ${this.name} the ${this.species}`);
+    super.greet("Meow");
   }
 
   feed() {
-    this.hunger -= 20;
-    console.log(`Yum, I love food`);
+    super.feed(this.food);
+  }
+
+  meow() {
+    console.log(`Meow`);
+  }
+}
+
+class Dog extends Animal {
+  constructor(name, color, hunger = 50) {
+    super(name, "dog", color, hunger);
+    this.food = "kibble";
+  }
+  greet() {
+    super.greet("Woof");
+  }
+
+  feed() {
+    super.feed(this.food);
   }
 }
 
@@ -32,4 +65,42 @@ class AnimalShelter {
     const animalIndex = this.animals.indexOf(animal);
     this.animals.splice(animalIndex, 1);
   }
+
+  getAnimalsBySpecies(species) {
+    return this.animals.filter((ani) => ani.species === species);
+  }
+}
+
+const shelter = new AnimalShelter();
+
+for (const animal of animalData) {
+  const hunger = animal.hunger ? animal.hunger : 50;
+
+  switch (animal.species) {
+    case "cat":
+      const newCat = new Cat(animal.name, animal.color, hunger);
+      shelter.addAnimal(newCat);
+      break;
+    case "dog":
+      const newDog = new Dog(animal.name, animal.color, hunger);
+      shelter.addAnimal(newDog);
+      break;
+    default:
+      const newAni = new Animal(
+        animal.name,
+        animal.species,
+        animal.color,
+        hunger
+      );
+      shelter.addAnimal(newAni);
+  }
+}
+
+// const mittens = new Cat("Mittens", "black", 75);
+// const fido = new Dog("Fido", "heeler blue");
+
+for (const ani of shelter.animals) {
+  console.log(ani.name);
+  console.log(ani.greet());
+  console.log(ani.feed());
 }
